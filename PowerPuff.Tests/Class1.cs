a4c7d3d5283171793db034a6a0720c75d3b04088
Stephan Dowding <sdowding@thoughtworks.com>
2016-08-23 15:54:26 +0800

    added test project for PowerPuff

---
+++ PowerPuff.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PowerPuff.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(1+1, Is.EqualTo(2));
+        }
+    }
+}
