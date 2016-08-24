89c3bb6db87bc185dd52b6c4c12496616c26df4f
David Nguyen <hungphuongjc@gmail.com>
2016-08-24 17:54:27 +0800

    fix failing build by adding dummy test to BlackWidow.Tests

---
+++ BlackWidow.Tests/SampleTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace BlackWidow.Tests
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(1 + 1, Is.EqualTo(2));
+        }
+    }
+}
