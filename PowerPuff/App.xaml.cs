93275f786df01e2dbe25abad51b50e87c76747d5
David Nguyen <hungphuongjc@gmail.com>
2016-08-24 17:42:42 +0800

    add global exception handling to PowerPuff

--- PowerPuff/App.xaml.cs
+++ PowerPuff/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Autofac;
+using BlackWidow.Logging;
 
 namespace PowerPuff
 {
@@ -17,6 +19,33 @@ namespace PowerPuff
         {
             var bootstrapper = new Bootstrapper();
             bootstrapper.Run();
+
+            var logger = bootstrapper.Container.Resolve<ILogger>();
+
+            //Unhandled exceptions from all threads in the AppDomain
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                LogUnhandledException(logger, (Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+            //Unhandled exceptions from main UI dispatcher thread
+            DispatcherUnhandledException += (s, e) =>
+            {
+                e.Handled = true;
+                LogUnhandledException(logger, e.Exception,
+                    "Application.Current.DispatcherUnhandledException");
+            };
+
+            //Unhandled exceptions from within each AppDomain that uses a task scheduler for async operations
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                e.SetObserved();
+                LogUnhandledException(logger, e.Exception, "TaskScheduler.UnobservedTaskException");
+            };
+        }
+
+        private void LogUnhandledException(ILogger logger, Exception exception, string eventName)
+        {
+            logger.Error(exception, eventName);
+            MessageBox.Show($"An exception occurred: {exception}");
         }
     }
 }
