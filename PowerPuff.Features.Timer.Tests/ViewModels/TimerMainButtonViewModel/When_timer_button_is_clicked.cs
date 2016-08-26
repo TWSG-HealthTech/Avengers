06e8625cd57eca90e431773b23e115688431f640
Tobie Wee <tobiewee@gmail.com>
2016-08-26 18:10:12 +0800

    Added tests for menu button click and back button click.

---
+++ PowerPuff.Features.Timer.Tests/ViewModels/TimerMainButtonViewModel/When_timer_button_is_clicked.cs
@@ -0,0 +1,32 @@
+﻿using Machine.Specifications;
+using PowerPuff.Common;
+using PowerPuff.Common.Events;
+using PowerPuff.Features.Timer.Views;
+using Prism.Events;
+using SUT = PowerPuff.Features.Timer.ViewModels;
+using M = Moq;
+
+namespace PowerPuff.Features.Timer.Tests.ViewModels.TimerMainButtonViewModel
+{
+    [Subject(typeof(SUT.TimerMainButtonViewModel))]
+    class When_timer_button_is_clicked
+    {
+        Establish context = () =>
+        {
+            _eventMock = new M.Mock<NavigationEvent>();
+            var eventAggregator = new M.Mock<IEventAggregator>();
+            eventAggregator.Setup(aggregator => aggregator.GetEvent<NavigationEvent>()).Returns(_eventMock.Object);
+            _subject = new SUT.TimerMainButtonViewModel(eventAggregator.Object);
+        };
+
+        Because of = () => _subject.GoToTimerPageCommand.Execute();
+
+        It should_request_to_go_to_the_timer_page = () =>
+            _eventMock.Verify(
+                e => e.Publish(new NavigationEventPayload(RegionNames.MainContentRegion,
+                    typeof(TimerMainView))));
+
+        private static SUT.TimerMainButtonViewModel _subject;
+        private static M.Mock<NavigationEvent> _eventMock;
+    }
+}
---
+++ PowerPuff.Features.Timer.Tests/ViewModels/TimerMainViewModel/When_back_button_in_timer_is_clicked.cs
@@ -0,0 +1,28 @@
+﻿using Machine.Specifications;
+using PowerPuff.Common.Events;
+using Prism.Events;
+using SUT = PowerPuff.Features.Timer.ViewModels;
+using M = Moq;
+
+namespace PowerPuff.Features.Timer.Tests.ViewModels.TimerMainViewModel
+{
+    [Subject(typeof(SUT.TimerMainViewModel))]
+    class When_back_button_in_timer_is_clicked
+    {
+        Establish context = () =>
+        {
+            _eventMock = new M.Mock<HomeNavigationEvent>();
+            var eventAggregatorMock = new M.Mock<IEventAggregator>();
+            eventAggregatorMock.Setup(aggregator => aggregator.GetEvent<HomeNavigationEvent>())
+                .Returns(_eventMock.Object);
+            _subject = new SUT.TimerMainViewModel(eventAggregatorMock.Object);
+        };
+
+        Because of = () => _subject.GoBackCommand.Execute();
+
+        private It should_request_to_go_back_to_home_view = () => _eventMock.Verify(e => e.Publish());
+
+        private static SUT.TimerMainViewModel _subject;
+        private static M.Mock<HomeNavigationEvent> _eventMock;
+    }
+}
