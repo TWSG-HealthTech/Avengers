9c8b83501098522a1e97719095d05cfbf66c0857
David Nguyen <hungphuongjc@gmail.com>
2016-09-01 09:41:11 +0800

    remove cors config since it's not necessary

--- RowdyRuff/Startup.cs
+++ RowdyRuff/Startup.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Cors.Internal;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,11 +29,6 @@ namespace RowdyRuff
         {
             // Add framework services.
             services.AddMvc();
-
-            services.Configure<MvcOptions>(options =>
-            {
-                options.Filters.Add(new CorsAuthorizationFilterFactory("AllowSpecificOrigin"));
-            });
 
             services.AddEntityFrameworkSqlServer()
                     .AddDbContext<RowdyRuffContext>(options =>
