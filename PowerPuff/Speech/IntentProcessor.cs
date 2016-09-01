f5a798401d3525b4fe90a5d081f968bc1ff37bc3
Stephan Dowding <sdowding@thoughtworks.com>
2016-09-01 16:00:48 +0800

    added intent processor

---
+++ PowerPuff/Speech/IntentProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace PowerPuff.Speech
+{
+    public class IntentProcessor
+    {
+        private readonly IIntentHandler _defaultHandler;
+        public ReadOnlyDictionary<string, IIntentHandler> _handlers;
+
+        public IntentProcessor(IEnumerable<IIntentHandler> intentHandlers, IIntentHandler defaultHandler)
+        {
+            _defaultHandler = defaultHandler;
+            _handlers = new ReadOnlyDictionary<string, IIntentHandler>(intentHandlers.ToDictionary(handler => handler.IntentName));
+        }
+
+        public void Process(string intentName)
+        {
+            IIntentHandler handler;
+            if (!_handlers.TryGetValue(intentName, out handler))
+            {
+                handler = _defaultHandler;
+            };
+            handler.Handle();
+        }
+    }
+
+    public interface IIntentHandler
+    {
+        string IntentName { get; }
+        void Handle();
+    }
+}
