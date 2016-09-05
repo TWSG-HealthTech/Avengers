8f47cfa95a7f66df244aa996ca3b6a8a70982a98
David Nguyen <hungphuongjc@gmail.com>
2016-09-05 12:37:52 +0800

    move RowdyRuff to separate repository

--- RowdyRuff.Core/Common/IClientProfileRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace RowdyRuff.Core.Common
-{
-    public interface IClientProfileRepository
-    {
-        ClientProfile FindProfileBy(string profileId);
-    }
-}
