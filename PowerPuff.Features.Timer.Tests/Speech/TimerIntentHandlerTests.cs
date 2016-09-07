464229cefecedb251bf2c6faf19bb48a1d5cf784
Stephan Dowding <sdowding@thoughtworks.com>
2016-09-07 14:32:00 +0800

    Timer intent interpret request

--- PowerPuff.Features.Timer.Tests/Speech/TimerIntentHandlerTests.cs
+++ PowerPuff.Features.Timer.Tests/Speech/TimerIntentHandlerTests.cs
@@ -1,31 +1,206 @@
 ﻿using Machine.Specifications;
-using Moq;
+using Microsoft.Cognitive.LUIS;
+using Newtonsoft.Json.Linq;
 using PowerPuff.Common.Speech;
 using PowerPuff.Features.Timer.Navigation;
 using PowerPuff.Features.Timer.Speech;
 using PowerPuff.Features.Timer.Tests.Navigation;
 using PowerPuff.Test.Helpers;
 using Prism.Regions;
+using M = Moq;
 
 namespace PowerPuff.Features.Timer.Tests.Speech
 {
     [Subject(typeof(TimerIntentHandler))]
     class TimerIntentHandlerTests
     {
-        private Establish context = () =>
+        protected static TimerIntentHandler _subject;
+        protected static M.Mock<ISpeechSynthesiser> _speechSynthesiserMock;
+        protected static M.Mock<IRegionManager> _regionManagerMock;
+
+        protected static LuisResult _luisResult;
+        protected static bool? _handled;
+
+        Establish context = () =>
         {
-            _regionManagerMock = new Mock<IRegionManager>();
+            _regionManagerMock = new M.Mock<IRegionManager>();
             var timerNavigator = new TimerNavigator(_regionManagerMock.Object, new TestDispatcher());
-            _speechSynthesiserMock = new Mock<ISpeechSynthesiser>();
+            _speechSynthesiserMock = new M.Mock<ISpeechSynthesiser>();
             _subject = new TimerIntentHandler(_speechSynthesiserMock.Object, timerNavigator);
         };
 
-        private Because of = async () => await _subject.SetTimer(null, null);
+        class Successful_Request : TimerIntentHandlerTests
+        {
 
-        Behaves_like<TimerNavigatorBehaviors> it_navigates_to_timer_view;
+            private Establish context = () =>
+            {
+                _luisResult = new LuisResult();
+                _luisResult.Load(JToken.Parse(@"
+{
+  ""query"": ""set a 2 hour timer"",
+  ""intents"": [
+    {
+                ""intent"": ""SetTimer"",
+      ""score"": 0.844260931,
+      ""actions"": [
+        {
+          ""triggered"": true,
+          ""name"": ""SetTimer"",
+          ""parameters"": [
+            {
+              ""name"": ""duration"",
+              ""required"": true,
+              ""value"": [
+                {
+                  ""entity"": ""2 hour"",
+                  ""type"": ""builtin.datetime.duration"",
+                  ""resolution"": {
+                    ""duration"": ""PT2H""
+                  }
+    }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      ""intent"": ""None"",
+      ""score"": 0.0204212964
+    }
+  ],
+  ""entities"": [
+    {
+      ""entity"": ""2 hour"",
+      ""type"": ""builtin.datetime.duration"",
+      ""startIndex"": 6,
+      ""endIndex"": 11,
+      ""resolution"": {
+        ""duration"": ""PT2H""
+      }
+    }
+  ]
+}
+"));
+            };
 
-        protected static TimerIntentHandler _subject;
-        protected static Mock<ISpeechSynthesiser> _speechSynthesiserMock;
-        protected static Mock<IRegionManager> _regionManagerMock;
+            private Because of = async () => _handled = await _subject.SetTimer(_luisResult, null);
+
+            Behaves_like<TimerNavigatorBehaviors> it_navigates_to_timer_view;
+
+            It handles_the_intent = () => _handled.ShouldEqual(true);
+
+            It responds_with_speech = () => _speechSynthesiserMock.Verify(ss => ss.Speak("Setting timer for 2 hours"));
+        }
+
+        class Incomplete_Request
+        {
+            private Establish context = () =>
+            {
+                _luisResult = new LuisResult();
+                _luisResult.Load(JToken.Parse(@"
+{
+  ""query"": ""set a 2 hour timer"",
+  ""intents"": [
+    {
+      ""intent"": ""SetTimer"",
+      ""score"": 0.844260931,
+      ""actions"": [
+        {
+          ""triggered"": false,
+          ""name"": ""SetTimer"",
+          ""parameters"": [
+            {
+              ""name"": ""duration"",
+              ""required"": true,
+              ""value"": null
+            }
+          ]
+        }
+      ]
+    },
+    {
+      ""intent"": ""None"",
+      ""score"": 0.0204212964
+    }
+  ],
+  ""entities"": [
+    {
+      ""entity"": ""2 hour"",
+      ""type"": ""builtin.datetime.duration"",
+      ""startIndex"": 6,
+      ""endIndex"": 11,
+      ""resolution"": {
+        ""duration"": ""PT2H""
+      }
+    }
+  ]
+}
+"));
+            };
+
+            private Because of = async () => _handled = await _subject.SetTimer(_luisResult, null);
+
+            It does_not_handle_the_intent = () => _handled.ShouldEqual(false);
+        }
+
+        class Invalid_Duration
+        {
+            private Establish context = () =>
+            {
+                _luisResult = new LuisResult();
+                _luisResult.Load(JToken.Parse(@"
+{
+  ""query"": ""set a 2 hour timer"",
+  ""intents"": [
+    {
+                ""intent"": ""SetTimer"",
+      ""score"": 0.844260931,
+      ""actions"": [
+        {
+          ""triggered"": true,
+          ""name"": ""SetTimer"",
+          ""parameters"": [
+            {
+              ""name"": ""duration"",
+              ""required"": true,
+              ""value"": [
+                {
+                  ""entity"": ""tomorrow"",
+                  ""type"": ""builtin.datetime.date"",
+                  ""resolution"": {
+                    ""date"": ""2016-09-08""
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      ""intent"": ""None"",
+      ""score"": 0.0204212964
+    }
+  ],
+  ""entities"": [
+    {
+     ""entity"": ""tomorrow"",
+      ""type"": ""builtin.datetime.date"",
+      ""startIndex"": 6,
+      ""endIndex"": 13,
+      ""resolution"": {
+          ""date"": ""2016-09-08""
+      }
+    }
+  ]
+}
+"));
+            };
+
+            private Because of = async () => _handled = await _subject.SetTimer(_luisResult, null);
+
+            It does_not_handle_the_intent = () => _handled.ShouldEqual(false);
+        }
     }
 }
--- PowerPuff.Features.Timer/Speech/TimerIntentHandler.cs
+++ PowerPuff.Features.Timer/Speech/TimerIntentHandler.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Cognitive.LUIS;
 using PowerPuff.Common.Speech;
 using PowerPuff.Features.Timer.Navigation;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace PowerPuff.Features.Timer.Speech
@@ -19,9 +21,38 @@ namespace PowerPuff.Features.Timer.Speech
         [IntentHandler(0, Name = "SetTimer")]
         public async Task<bool> SetTimer(LuisResult result, object context)
         {
+            var resolution = result.Intents.First(i => i.Name == "SetTimer")?
+                .Actions.FirstOrDefault(x => x.Name == "SetTimer" && x.Triggered)?
+                .Parameters.FirstOrDefault(x => x.Name == "duration")?
+                .ParameterValues.FirstOrDefault(p => p.Type == "builtin.datetime.duration")?
+                .Resolution["duration"] as string;
+            if (resolution == null) return false;
+
+            var duration = System.Xml.XmlConvert.ToTimeSpan(resolution);
+            Console.WriteLine(duration);
+
             _timerNavigator.GoToTimerPage();
-            _speechSynthesiser.Speak("Setting timer");
+            _speechSynthesiser.Speak($"Setting timer for{Speechify(duration)}");
             return true;
+        }
+
+        private string Speechify(TimeSpan duration)
+        {
+            var speech = string.Empty;
+            speech += Speechify(duration.Hours, "hour");
+            speech += Speechify(duration.Minutes, "minute");
+            speech += Speechify(duration.Seconds, "second");
+            return speech;
+        }
+
+        private string Speechify(int quantity, string unit)
+        {
+            return quantity > 0 ? $" {quantity} {unit}{s(quantity)}" : string.Empty;
+        }
+
+        private string s(int value)
+        {
+            return value == 1 ? string.Empty : "s";
         }
     }
 }
