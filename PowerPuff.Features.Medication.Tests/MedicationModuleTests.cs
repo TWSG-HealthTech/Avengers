e80ce0b329b33662432c4be4d78da7744ee7830a
iyzhang <iyzhang@thoughtworks.com>
2016-09-13 17:00:11 +0800

    add medication main button.

--- PowerPuff.Features.Medication.Tests/MedicationModuleTests.cs
+++ PowerPuff.Features.Medication.Tests/MedicationModuleTests.cs
@@ -2,6 +2,8 @@
 using Machine.Specifications;
 using Moq;
 using PowerPuff.Common;
+using PowerPuff.Features.Medication;
+using PowerPuff.Features.Medication.Views;
 using Prism.Regions;
 using It = Machine.Specifications.It;
 
@@ -9,6 +11,25 @@ namespace PowerPuff.Features.Medication.Tests
 {
     public class MedicationModuleTests
     {
-        
+        [Subject(typeof(MedicationModule))]
+        public class When_initialize_medication_module
+        {
+            private static MedicationModule _subject;
+            private static Mock<IRegionManager> _regionManagerMock;
+            private static IContainer _container;
+
+            private Establish context = () =>
+            {
+                _regionManagerMock = new Mock<IRegionManager>();
+                var containerBuilder = new ContainerBuilder();
+                _container = containerBuilder.Build();
+                _subject = new MedicationModule(_regionManagerMock.Object, _container);
+            };
+
+            Because of = () => _subject.Initialize();
+
+            It should_register_view_with_main_button_region = () =>
+                _regionManagerMock.Verify(manager => manager.RegisterViewWithRegion(RegionNames.MainButtonsRegion, typeof(MedicationMainButtonView)));
+        }
     }
 }
--- PowerPuff.Features.Medication/MedicationModule.cs
+++ PowerPuff.Features.Medication/MedicationModule.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autofac;
+using PowerPuff.Common;
+using PowerPuff.Features.Medication.Views;
+using Prism.Modularity;
+using Prism.Mvvm;
+using Prism.Regions;
 
 namespace PowerPuff.Features.Medication
 {
-    public class Class1
+    public class MedicationModule : IModule
     {
+        private readonly IContainer _container;
+        private readonly IRegionManager _regionManager;
+
+        public MedicationModule(IRegionManager regionManager, IContainer container)
+        {
+            _container = container;
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            ConfigureDependencies();
+
+            _regionManager.RegisterViewWithRegion(RegionNames.MainButtonsRegion, typeof(MedicationMainButtonView));
+
+            ViewModelLocationProvider.SetDefaultViewModelFactory(type => _container.Resolve(type));
+        }
+
+        private void ConfigureDependencies()
+        {
+            var updater = new ContainerBuilder();
+
+            updater.Update(_container);
+        }
     }
 }
