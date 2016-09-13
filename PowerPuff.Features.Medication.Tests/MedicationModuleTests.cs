e63347d810a6f4df8467339d3b1d3cf168c342dd
iyzhang <iyzhang@thoughtworks.com>
2016-09-13 14:43:54 +0800

    add Medication module

---
+++ PowerPuff.Features.Medication.Tests/MedicationModuleTests.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Machine.Specifications;
+using Moq;
+using PowerPuff.Common;
+using Prism.Regions;
+using It = Machine.Specifications.It;
+
+namespace PowerPuff.Features.Medication.Tests
+{
+    public class MedicationModuleTests
+    {
+        
+    }
+}
