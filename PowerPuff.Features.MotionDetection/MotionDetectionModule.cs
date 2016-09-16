472b6c7b47aab3712c683d4ede9473e578244035
Stephan Dowding <sdowding@thoughtworks.com>
2016-09-16 14:29:19 +0800

    Actual Motion Detection with Camera

--- PowerPuff.Features.MotionDetection/MotionDetectionModule.cs
+++ PowerPuff.Features.MotionDetection/MotionDetectionModule.cs
@@ -34,7 +34,7 @@ namespace PowerPuff.Features.MotionDetection
 
             updater.RegisterType<MotionDetectionModel>().As<IMotionDetectionModel>().SingleInstance();
             updater.RegisterType<SettingsViewModel>();
-            updater.RegisterType<MotionDetector>().As<IMotionDetector>().SingleInstance();
+            updater.RegisterType<CameraMotionDetector>().As<IMotionDetector>().SingleInstance();
             updater.RegisterType<Timer>().As<ITimer>().SingleInstance();
             updater.RegisterType<Alerter>().SingleInstance();
 
--- PowerPuff.Features.MotionDetection/Services/IMotionDetector.cs
+++ PowerPuff.Features.MotionDetection/Services/IMotionDetector.cs
@@ -1,9 +1,43 @@
-﻿using System;
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+using System;
+using System.Linq;
 
 namespace PowerPuff.Features.MotionDetection.Services
 {
-    public class MotionDetector : IMotionDetector {
+    public class CameraMotionDetector : IMotionDetector, IDisposable
+    {
+        private readonly MotionDetector _detector =
+           new MotionDetector(
+              new TwoFramesDifferenceDetector(true));
+
+        public CameraMotionDetector()
+        {
+            var info = new FilterInfoCollection(FilterCategory.VideoInputDevice).OfType<FilterInfo>().FirstOrDefault();
+            _source = new VideoCaptureDevice(info.MonikerString);
+            _source.NewFrame += VideoSourcePlayerNewFrame;
+            _source.Start();
+        }
+
+        private void VideoSourcePlayerNewFrame(object sender, NewFrameEventArgs eventargs)
+        {
+            var motionValue = _detector.ProcessFrame(eventargs.Frame);
+            if (motionValue > 0)
+            {
+                MotionDetected?.Invoke();
+            }
+        }
+
+        private IVideoSource _source;
+
         public event Action MotionDetected;
+
+        public void Dispose()
+        {
+            _source.SignalToStop();
+            _source.WaitForStop();
+        }
     }
 
     public interface IMotionDetector
