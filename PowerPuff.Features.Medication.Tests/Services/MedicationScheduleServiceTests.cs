15f4dfe0706f5071f95ca170954e4195227181cf
Ying Zhang <zhangying87@gmail.com>
2016-09-20 17:16:29 +0800

    Schedule the first reminder to next day if its time is before now.

--- PowerPuff.Features.Medication.Tests/Services/MedicationScheduleServiceTests.cs
+++ PowerPuff.Features.Medication.Tests/Services/MedicationScheduleServiceTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Machine.Specifications;
 using Moq;
+using PowerPuff.Common.Helpers;
 using PowerPuff.Features.Medication.Models;
 using PowerPuff.Features.Medication.Services;
 using It = Machine.Specifications.It;
@@ -12,14 +13,18 @@ namespace PowerPuff.Features.Medication.Tests.Services
     public class MedicationScheduleServiceTests
     {
         private static Mock<IJobScheduler> _schedulerMock;
+        private static Mock<IDateTimeProvider> _dateTimeProviderMock;
         private static MedicationScheduleService _subject;
         private static MedicationSchedule _schedule;
         private static IList<MedicationSchedule> _activatedSchedules;
+        private static DateTime _now;
+        private static DateTime _expectedFirstRunTime;
 
         private Establish context = () =>
         {
             _schedulerMock = new Mock<IJobScheduler>();
-            _subject = new MedicationScheduleService(_schedulerMock.Object);
+            _dateTimeProviderMock = new Mock<IDateTimeProvider>();
+            _subject = new MedicationScheduleService(_schedulerMock.Object, _dateTimeProviderMock.Object);
             _activatedSchedules = new List<MedicationSchedule>();
             _subject.OnMedicationSchedule += s => _activatedSchedules.Add(s);
             _schedule = new MedicationSchedule
@@ -30,16 +35,42 @@ namespace PowerPuff.Features.Medication.Tests.Services
             };
         };
 
-        private class When_add_schedule
+        private class When_add_schedule_whose_time_is_before_now
         {
+            private Establish context = () =>
+            {
+                _now = new DateTime(1990, 11, 11, 14, 0, 0);
+                _expectedFirstRunTime = new DateTime(1990, 11, 12, 12, 30, 0);
+                _dateTimeProviderMock.Setup(p => p.Now).Returns(_now);
+            };
+
             private Because of = () => _subject.AddSchedule(_schedule);
 
             private It should_has_new_schedule = () => _subject.GetAllSchedules().ShouldContain(_schedule);
 
-            private It should_schedule_new_job_with_job_scheduler =
+            private It should_schedule_new_job_starting_from_next_day =
                 () =>
                     _schedulerMock.Verify(
-                        s => s.AddDailySchedule(_schedule.Name, _schedule.Hour, _schedule.Minute));
+                        s => s.AddDailySchedule(_schedule.Name, _expectedFirstRunTime));
+        }
+
+        private class When_add_schedule_whose_time_is_after_now
+        {
+            private Establish context = () =>
+            {
+                _now = new DateTime(1990, 11, 11, 10, 20, 0);
+                _expectedFirstRunTime = new DateTime(1990, 11, 11, 12, 30, 0);
+                _dateTimeProviderMock.Setup(p => p.Now).Returns(_now);
+            };
+
+            private Because of = () => _subject.AddSchedule(_schedule);
+
+            private It should_has_new_schedule = () => _subject.GetAllSchedules().ShouldContain(_schedule);
+
+            private It should_schedule_new_job_starting_from_same_day =
+                () =>
+                    _schedulerMock.Verify(
+                        s => s.AddDailySchedule(_schedule.Name, _expectedFirstRunTime));
         }
 
         private class When_schedule_is_triggered
--- PowerPuff.Features.Medication/Services/MedicationScheduleService.cs
+++ PowerPuff.Features.Medication/Services/MedicationScheduleService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using FluentScheduler;
+using PowerPuff.Common.Helpers;
 using PowerPuff.Features.Medication.Models;
 using PowerPuff.Features.Medication.ViewModels;
 
@@ -9,12 +10,14 @@ namespace PowerPuff.Features.Medication.Services
     public class MedicationScheduleService : IMedicationScheduleService
     {
         private readonly IJobScheduler _scheduler;
+        private readonly IDateTimeProvider _dateTimeProvider;
         private readonly Dictionary<string, MedicationSchedule> _scheduleStore;
 
-        public MedicationScheduleService(IJobScheduler scheduler)
+        public MedicationScheduleService(IJobScheduler scheduler, IDateTimeProvider dateTimeProvider)
         {
             _scheduler = scheduler;
             _scheduler.OnSchedule += _scheduler_OnSchedule;
+            _dateTimeProvider = dateTimeProvider;
             _scheduleStore = new Dictionary<string, MedicationSchedule>();
         }
 
@@ -28,7 +31,16 @@ namespace PowerPuff.Features.Medication.Services
         public void AddSchedule(MedicationSchedule schedule)
         {
             _scheduleStore.Add(schedule.Name, schedule);
-            _scheduler.AddDailySchedule(schedule.Name, schedule.Hour, schedule.Minute);
+            _scheduler.AddDailySchedule(schedule.Name, CalculateFirstRunTime(schedule.Hour, schedule.Minute));
+        }
+
+        private DateTime CalculateFirstRunTime(int hour, int minute)
+        {
+            var now = _dateTimeProvider.Now;
+            var scheduledTimeInSameDay = new DateTime(now.Year, now.Month, now.Day, hour, minute, 0);
+            return scheduledTimeInSameDay > now
+                ? scheduledTimeInSameDay
+                : scheduledTimeInSameDay.AddDays(1);
         }
 
         public void RemoveSchedule(MedicationSchedule schedule)
@@ -47,10 +59,17 @@ namespace PowerPuff.Features.Medication.Services
     {
         public event Action<string> OnSchedule;
 
-        public void AddDailySchedule(string id, int hour, int minute)
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public JobScheduler(IDateTimeProvider dateTimeProvider)
+        {
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public void AddDailySchedule(string id, DateTime firstRunTime)
         {
             JobManager.AddJob(() => OnSchedule?.Invoke(id),
-                s => s.WithName(id).ToRunOnceAt(hour, minute).AndEvery(24).Hours());
+                s => s.WithName(id).ToRunOnceAt(firstRunTime).AndEvery(24).Hours());
         }
 
         public void RemoveDailySchedule(string id)
@@ -62,7 +81,7 @@ namespace PowerPuff.Features.Medication.Services
     public interface IJobScheduler
     {
         event Action<string> OnSchedule;
-        void AddDailySchedule(string id, int hour, int minute);
+        void AddDailySchedule(string id, DateTime firstRunTime);
         void RemoveDailySchedule(string id);
     }
 }
